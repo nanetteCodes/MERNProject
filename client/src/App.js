24fe96948ca1087ed8f0d2ff1ef0732144898c8d
Nanette <njulius89@gmail.com>
2019-09-16 21:41:33 -0700

    loging and reg ui

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+const App = () =>
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </>
+  </Router>
+
+
+
+
+export default App;
