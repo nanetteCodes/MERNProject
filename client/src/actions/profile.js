e36d27ee13d4d23fb365d6178eb49f75376cc976
Nanette <nanette.codes@gmail.com>
2020-01-28 12:45:53 -0800

    frm

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { setAlert } from './alert';
 
 import {
   GET_PROFILE,
@@ -14,8 +15,41 @@ export const getCurrentProfile = () => async dispatch => {
       type: GET_PROFILE,
       payload: res.data
     });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
   }
-  catch (err) {
+}
+
+// Create or update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+   // redirect for new profile, stay on same page for edit profile
+   if(!edit) {
+     history.push('/dashboard')
+   }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile'
 import Spinner from '../layouts/Spinner';
+import DashboardActions from './DashboardActions';
 
 const Dashboard = ({ getCurrentProfile, auth: {user}, profile: {profile, loading} }) => {
   useEffect(() => {
@@ -15,15 +16,16 @@ const Dashboard = ({ getCurrentProfile, auth: {user}, profile: {profile, loading
     <Fragment>
       <h1 className="large text-primary">Dashboard</h1>
       <p className="lead"><i className="fas fa-user"></i> Welcome { user && user.name }</p>
-      {profile != null ? <Fragment>has</Fragment> : 
-      
-      <Fragment> 
-        <p>You have not yet setup your profile, please add some info</p> 
-        <Link to='/create-profile' className="btn btn-primary my-1">
-          Create Profile
-        </Link>
-      </Fragment>}
-
+      {profile != null ? (
+        <Fragment><DashboardActions /></Fragment>
+      ) : (   
+        <Fragment> 
+          <p>You have not yet setup your profile, please add some info</p> 
+          <Link to='/create-profile' className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
     </Fragment>
   );
 }
