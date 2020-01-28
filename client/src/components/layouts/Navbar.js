4d31e0945886a4b5df7b4e663cd6ff4cf7e1aafe
Nanette <nanette.codes@gmail.com>
2020-01-27 16:35:39 -0800

    fixed deprication errors, starting on dashboard

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -7,10 +7,17 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
-      <li><a onClick={logout} href="#!">
-        <i className="fas fa-sign-out-alt"></i>{' '}
-        <span className='hide-sm'>Logout</span>
-      </a>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-sign-out-alt"></i>{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
       </li>
     </ul>
   );
