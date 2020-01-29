e56892c54b263b34bc708fac87c9eb267b0c4f1d
Nanette <nanette.codes@gmail.com>
2020-01-29 10:32:53 -0800

    dne

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,14 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import profile from './profile';
+import post from './post';
+
 
 
 // object of any reducers we create
 export default combineReducers({
   alert,
   auth,
-  profile
+  profile,
+  post
 });
